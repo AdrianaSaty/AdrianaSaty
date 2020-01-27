87831a555a67d71b7b67e8638dcc7692db19ccbb
Adriana Saty <adri-saty@hotmail.com>
2020-01-27 17:16:06 -0300

    starting project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar /> */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
