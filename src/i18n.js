ff6d502438841f3c353210e8ff3449cef1c6a3ce
Adriana Saty Bertelli <adrianasaty@MacBook-Pro-de-Adriana.local>
2020-05-07 02:37:27 -0300

    comment unused import

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 const fallbackLng = ['en'];
