da027161ac8dc28a6cf6ac1036d3a6e2326f8f34
Jo_ao <joaopedro.viana07@gmail.com>
2022-04-26 11:03:12 -0300

    fix: accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,87 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Accordion.scss';
 import arrow from '../../img/svgs/arrow.svg';
 
-function expand() {
-    const text = document.getElementById('text');
-    const rotArrow = document.getElementById('rotArrow');
 
-     document.getElementById('accordion')?.addEventListener('click', function () {
-         if (text?.classList.contains('collapsed')) {
-             rotArrow?.classList.add('arrow-expanded')
-             text?.classList.remove('collapsed');
-             text?.classList.add('expanded');
-         }
-         else {
-             rotArrow?.classList.remove('arrow-expanded')
-             text?.classList.remove('expanded');
-             text?.classList.add('collapsed');
-         }
-     })
-    
-    const text2 = document.getElementById('text-2');
-    const rotArrow2 = document.getElementById('rotArrow-2');
-    
-    document.getElementById('accordion-2')?.addEventListener('click', function () {
-        if (text2?.classList.contains('collapsed')) {
-            rotArrow2?.classList.add('arrow-expanded')
-            text2?.classList.remove('collapsed');
-            text2?.classList.add('expanded');
-        }
-        else {
-            rotArrow2?.classList.remove('arrow-expanded')
-            text2?.classList.remove('expanded');
-            text2?.classList.add('collapsed');
-        }
-    })
-
-    const text3 = document.getElementById('text-3');
-    const rotArrow3 = document.getElementById('rotArrow-3');
-    
-    document.getElementById('accordion-3')?.addEventListener('click', function () {
-        if (text3?.classList.contains('collapsed')) {
-            rotArrow3?.classList.add('arrow-expanded')
-            text3?.classList.remove('collapsed');
-            text3?.classList.add('expanded');
-        }
-        else {
-            rotArrow3?.classList.remove('arrow-expanded')
-            text3?.classList.remove('expanded');
-            text3?.classList.add('collapsed');
-        }
-    })
-
-}
+const info = [
+  {
+    question: "Teste",
+    answer:
+      "Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium. Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium.Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium.",
+  },
+  {
+    question: "Teste",
+    answer:
+      "Aperiam ab atque incidunt dolores ullam est, earum ipsa recusandae velit cumque. Aperiam ab atque incidunt dolores ullam est, earum ipsa recusandae velit cumque.",
+  },
+  {
+    question: "Teste",
+    answer:
+      "Blanditiis aliquid adipisci quisquam reiciendis voluptates itaque.",
+  },
+];
 
 function Accordion() {
+
+    const [selected, setSelected] = useState(null)
+
+    const toggle = (i:any) => {
+        if (selected === i) {
+            return setSelected(null)
+        }
+        setSelected(i)
+    }
+
+
     return (
-        <div className='container'>
-            <div className='btn'>
-                <button id='accordion' onClick={expand}>Teste
-                    <img src={arrow} id='rotArrow' className="arrow"></img>
-                </button>
-                <div className='collapsed' id='text'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum officia, inventore, magnam quaerat suscipit modi labore, eum quo est iste molestias! Nobis sunt asperiores blanditiis at porro voluptatem maxime itaque.</p>
-                </div>
-            </div>
-            <div className='btn'>
-                <button id='accordion-2' onClick={expand}>Teste
-                    <img src={arrow} id='rotArrow-2' className="arrow"></img>
-                </button>
-                <div className='collapsed' id='text-2'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum officia, inventore, magnam quaerat suscipit modi labore, eum quo est iste molestias! Nobis sunt asperiores blanditiis at porro voluptatem maxime itaque.</p>
-                </div>
-            </div>
-            <div className='btn'>
-                <button id='accordion-3' onClick={expand}>Teste
-                    <img src={arrow} id='rotArrow-3' className="arrow"></img>
-                </button>
-                <div className='collapsed' id='text-3'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum officia, inventore, magnam quaerat suscipit modi labore, eum quo est iste molestias! Nobis sunt asperiores blanditiis at porro voluptatem maxime itaque.</p>
-                </div>
+        <div className='wrapper'>
+            <div className='accordion'>
+                {info.map((item, i) => (
+                    <div className='item'>
+                        <div className='title' onClick={() => toggle(i)}>
+                            <h2>{item.question}</h2>
+                            <span>
+                                {selected === i ? <img src={arrow} className='arrow-expanded'></img> : <img src={arrow} className='arrow-collapsed'></img>}
+                            </span>
+                        </div>
+                        <div className={selected === i ? 'content show' : 'content'}>{item.answer}</div>
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    ) 
 }
 
 export default Accordion;
