3857859ffa26b69d8a17fe4b8ed15bf73df5f238
AdrianaSaty <adri-saty@hotmail.com>
2022-04-26 21:34:01 -0300

    feat: ajust accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,55 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Accordion.scss';
 import arrow from '../../img/svgs/arrow.svg';
 
+interface Props {
+    title: string,
+    description: string
+}
+function Accordion(props: Props) {
+    const { title, description } = props;
 
-const info = [
-  {
-    question: "Teste",
-    answer:
-      "Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium. Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium.Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium.",
-  },
-  {
-    question: "Teste",
-    answer:
-      "Aperiam ab atque incidunt dolores ullam est, earum ipsa recusandae velit cumque. Aperiam ab atque incidunt dolores ullam est, earum ipsa recusandae velit cumque.",
-  },
-  {
-    question: "Teste",
-    answer:
-      "Blanditiis aliquid adipisci quisquam reiciendis voluptates itaque.",
-  },
-];
-
-function Accordion() {
-
-    const [selected, setSelected] = useState(null)
-
-    const toggle = (i:any) => {
-        if (selected === i) {
-            return setSelected(null)
-        }
-        setSelected(i)
-    }
-
+    const [isSelected, setSelected] = useState(false)
 
     return (
         <div className='wrapper'>
             <div className='accordion'>
-                {info.map((item, i) => (
-                    <div className='item'>
-                        <div className='title' onClick={() => toggle(i)}>
-                            <h2>{item.question}</h2>
+                <div className="accordion">
+                    <div className="item">
+                        <div className="title" onClick={() => setSelected(!isSelected)}>
+                            <div>{title}</div>
                             <span>
-                                {selected === i ? <img src={arrow} className='arrow-expanded'></img> : <img src={arrow} className='arrow-collapsed'></img>}
+                                {isSelected ? <img src={arrow} className='arrow-expanded'></img> : <img src={arrow} className='arrow-collapsed'></img>}
                             </span>
                         </div>
-                        <div className={selected === i ? 'content show' : 'content'}>{item.answer}</div>
+                        {isSelected && <div>{description}</div>}
                     </div>
-                ))}
+                </div>
             </div>
         </div>
-    ) 
+    )
 }
 
 export default Accordion;
--- src/pages/SoftskillsCheckout/SoftskillsCheckout.tsx
+++ src/pages/SoftskillsCheckout/SoftskillsCheckout.tsx
@@ -3,11 +3,37 @@ import './SoftskillsCheckout.scss';
 import '../../components/Accordion/Accordion'
 import Accordion from '../../components/Accordion/Accordion';
 
+interface Question {
+    title: string,
+    description: string
+}
+
+interface Questions extends Array<Question>{}
+
+const questions: Questions = [
+    {
+        title: "Teste",
+        description:
+            "Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium. Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium.Tenetur ullam rerum ad iusto possimus sequi mollitia dolore sunt quam praesentium.",
+    },
+    {
+        title: "Teste",
+        description:
+            "Aperiam ab atque incidunt dolores ullam est, earum ipsa recusandae velit cumque. Aperiam ab atque incidunt dolores ullam est, earum ipsa recusandae velit cumque.",
+    },
+    {
+        title: "Teste",
+        description:
+            "Blanditiis aliquid adipisci quisquam reiciendis voluptates itaque.",
+    },
+];
+
 function SoftskillsCheckout() {
     return (
         <div>
-            ...
-            <Accordion />
+            {questions.map((item: Question, i: number) => (
+                <Accordion title={item.title} description={item.description} key={i}/>
+            ))}
         </div>
     )
 }
